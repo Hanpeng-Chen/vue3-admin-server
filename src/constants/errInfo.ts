e38ed47a803c6558dfff155da7342665a1e9d5b7
HanpengChen <hanpengchen07@163.com>
2021-06-15 18:04:50 +0800

    add error info config file

---
+++ src/constants/errInfo.ts
@@ -0,0 +1,46 @@
+export default {
+  registerUserNameExistInfo: {
+    code: 10001,
+    message: '用户名已存在'
+  },
+  registerFailInfo: {
+    code: 10002,
+    message: '注册失败，请重试'
+  },
+  registerUserNameNotExistInfo: {
+    code: 1003,
+    message: '用户名不存在'
+  },
+  loginFailInfo: {
+    code: 10004,
+    message: '登陆失败，用户名或密码错误'
+  },
+  loginCheckFailInfo: {
+    code: 10005,
+    message: '您尚未登录'
+  },
+  changePasswordFailInfo: {
+    code: 10006,
+    message: '修改密码失败，请重试'
+  },
+  getUserInfoFailInfo: {
+    code: 10007,
+    message: '用户信息获取失败'
+  },
+  getUserListFailInfo: {
+    code: 10008,
+    message: '用户列表获取失败，请重试'
+  },
+  editUserInfoFailInfo: {
+    code: 10009,
+    message: '用户信息修改失败，请重试'
+  },
+  deleteUserInfoFailInfo: {
+    code: 10010,
+    message: '用户删除失败，请重试'
+  },
+  updateUserRoleFailInfo: {
+    code: 10011,
+    message: '用户角色修改失败，请重试'
+  }
+}
