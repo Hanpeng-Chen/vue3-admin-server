9a8e675b60ae2c040873d441be7e40889b44bc96
HanpengChen <hanpengchen07@163.com>
2021-07-22 15:51:43 +0800

    feat: login interface(some error tip is still unfixed)

--- src/controller/auth.ts
+++ src/controller/auth.ts
@@ -3,12 +3,15 @@ import { createUser, getUserInfo } from "../services/auth";
 import errInfo from "../constants/errInfo";
 import { ErrorResponse, SuccessResponse } from "../utils/Response";
 import { createMD5 } from "../utils/createMD5";
+import { createToken } from "../utils/token";
 
 const {
   registerUserNameExistInfo,
-  registerFailInfo
+  registerFailInfo,
+  loginFailInfo
 } = errInfo
 
+// 注册controller
 export const registerController = async (params: RegisterModel) => {
   const { username, password } = params;
   const userInfo = await getUserInfo({ username })
@@ -30,4 +33,27 @@ export const registerController = async (params: RegisterModel) => {
     const { code, message } = registerFailInfo
     return new ErrorResponse(code, message)
   }
+}
+
+// 登录controller
+interface LoginModel {
+  username: string;
+  password: string;
+}
+
+export const loginController = async (params: LoginModel) => {
+  const { username, password } = params;
+  // 根据用户名和密码，获取用户信息
+  const userInfo = await getUserInfo({ username, password });
+  if (userInfo) {
+    const { id, username } = userInfo;
+    const token = createToken({
+      id,
+      username
+    })
+    return new SuccessResponse({ token })
+  }
+  // 未获取到用户信息，登录失败
+  const { code, message } = loginFailInfo
+  return new ErrorResponse(code, message);
 }
--- src/routes/auth.ts
+++ src/routes/auth.ts
@@ -1,5 +1,6 @@
 import Router from '@koa/router'
-import { registerController } from '../controller/auth'
+import { loginController, registerController } from '../controller/auth'
+import { RegisterModel } from '../db/models/user'
 
 const router = new Router({
   prefix: '/api/auth'
@@ -10,7 +11,13 @@ const router = new Router({
  * /auth/register
  */
 router.post('/register', async ctx => {
-  ctx.body = await registerController(ctx.request.body)
+  ctx.body = await registerController(ctx.request.body as RegisterModel)
+})
+
+// 登录接口
+router.post('/login', async ctx => {
+  const { username, password } = ctx.request.body
+  ctx.body = await loginController({ username, password })
 })
 
 export default router
